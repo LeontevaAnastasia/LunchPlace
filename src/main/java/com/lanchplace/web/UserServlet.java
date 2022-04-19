0c33ca5d90d5cd4a489666bfddae5af949589291
AnastasiaLeonteva123 <lilacviolin@rambler.ru>
2022-04-19 19:51:24 +0300

    add userServlet, logging

---
+++ src/main/java/com/lanchplace/web/UserServlet.java
@@ -0,0 +1,21 @@
+package com.lanchplace.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+import static org.slf4j.LoggerFactory.getLogger;
+
+
+public class UserServlet extends HttpServlet {
+    private static final Logger log = getLogger(UserServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to users");
+        response.sendRedirect("users.jsp");
+    }
+}
+
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,13 @@
-<html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Java Enterprise (LunchPlace)</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h3>Проект <a href="https://github.com/LeontevaAnastasia/LunchPlace" target="_blank">Java Enterprise (LunchPlace)</a></h3>
+<hr>
+<ul>
+    <li><a href="users">Users</a></li>
+</ul>
 </body>
 </html>
